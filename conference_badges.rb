2ccad1f906500b9e9b8ffbe0bc065b4720261ec5
Kevin Dole <dole.kevin@gmail.com>
2020-09-21 13:03:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+speaker_names = ["Edsger", "Ada","Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(speaker_names)
+"Hello, my name is #{speaker_names}"
+end
+
+  
